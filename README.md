d94638eed3a6ef4f1ad87af496d26ed84ba9b945
hello@frances.ink <jeninedrew@gmail.com>
2018-07-30 12:14:49 -0400

    add gulp-modernizer

--- README.md
+++ README.md
@@ -1,15 +1,23 @@
 # A super simple starter.
 
 ## Gulp
+
 - [Sass](https://www.npmjs.com/package/gulp-sass)
-- [Browsersync](https://browsersync.io/docs/gulp) 
+- [Browsersync](https://browsersync.io/docs/gulp)
 - [Babel](https://www.npmjs.com/package/gulp-babel)
+- [Modernizer](https://www.npmjs.com/package/gulp-modernizr)
 
 ## Normalize
+
 - [normalize.css](https://necolas.github.io/normalize.css/)
 
-## Get started! 
+## Get started!
+
 - Pull this repo into your project directory `git clone https://github.com/jeninedrew/simple`
 - You're starting out fresh. Are these the package versions we want to go ahead with? If not, grab the most recent versions (and update starter).
 - OK, install packages! `npm install`
-- And get the project going. `gulp` 
+- And get the project going. `gulp`
+
+## TO DO
+
+- Double check modernizer is working as expected
--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,47 @@
 // Include gulp
-var gulp = require('gulp');
+var gulp = require("gulp");
 
 // Include plugins
-var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
-var babel = require('gulp-babel');
+var browserSync = require("browser-sync").create();
+var sass = require("gulp-sass");
+var babel = require("gulp-babel");
+var modernizr = require("gulp-modernizr");
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+gulp.task("serve", ["sass"], function() {
+  browserSync.init({
+    server: "./app"
+  });
 
-    browserSync.init({
-        server: "./app"
-    });
-
-    gulp.watch("app/scss/*.scss", ['sass']);
-    gulp.watch("app/*.html").on('change', browserSync.reload);
+  gulp.watch("app/scss/*.scss", ["sass"]);
+  gulp.watch("app/*.html").on("change", browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-    return gulp.src("app/scss/*.scss")
-        .pipe(sass())
-        .pipe(gulp.dest("app/css"))
-        .pipe(browserSync.stream());
+gulp.task("sass", function() {
+  return gulp
+    .src("app/scss/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("app/css"))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('default', () =>
-    gulp.src('src/app.js')
-        .pipe(babel({
-            presets: ['env']
-        }))
-        .pipe(gulp.dest('dist'))
+gulp.task("default", () =>
+  gulp
+    .src("src/app.js")
+    .pipe(
+      babel({
+        presets: ["env"]
+      })
+    )
+    .pipe(gulp.dest("dist"))
 );
 
-gulp.task('default', ['serve']);
+gulp.task("modernizr", function() {
+  return gulp
+    .src("./js/*.js")
+    .pipe(modernizr())
+    .pipe(gulp.dest("build/"));
+});
+
+gulp.task("default", ["serve"]);
